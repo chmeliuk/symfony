9d8180f24265b9caeab446ed9c0239d763508e85
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-10-26 14:49:39 +0200

    Request instead of RequestStack for Symfony <= 2.3

--- EventListener/DefineLocaleEventListener.php
+++ EventListener/DefineLocaleEventListener.php
@@ -2,22 +2,22 @@
 
 namespace PhpBenchmarksSymfony\EventListener;
 
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Translation\TranslatorInterface;
 
 class DefineLocaleEventListener
 {
     const EVENT_NAME = 'defineLocale';
 
-    /** @var RequestStack */
-    protected $requestStack;
+    /** @var Request */
+    protected $request;
 
     /** @var TranslatorInterface */
     protected $translator;
 
-    public function __construct(RequestStack $requestStack, TranslatorInterface $translator)
+    public function __construct(Request $request, TranslatorInterface $translator)
     {
-        $this->requestStack = $requestStack;
+        $this->request = $request;
         $this->translator = $translator;
     }
 
@@ -26,7 +26,7 @@ class DefineLocaleEventListener
         $locales = ['fr_FR', 'en_GB', 'aa_BB'];
         $locale = $locales[rand(0, 2)];
 
-        $this->requestStack->getCurrentRequest()->setLocale($locale);
+        $this->request->setLocale($locale);
         $this->translator->setLocale($locale);
     }
 }
